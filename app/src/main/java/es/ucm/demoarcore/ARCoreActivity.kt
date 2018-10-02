b3dbbf7eace71ec7f6385ef988f117a4452f6a54
Daniel Calle <danielcallesanchez@gmail.com>
2018-10-02 22:34:42 +0200

    Connection between two activities
    A button is implemented in the activity main that calls ARCore
    ARCore implements its xml but not its logic

---
+++ app/src/main/java/es/ucm/demoarcore/ARCoreActivity.kt
@@ -0,0 +1,14 @@
+package es.ucm.demoarcore
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class ARCoreActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_arcore)
+    }
+
+}
--- app/src/main/java/es/ucm/demoarcore/MainActivity.kt
+++ app/src/main/java/es/ucm/demoarcore/MainActivity.kt
@@ -1,12 +1,24 @@
 package es.ucm.demoarcore
 
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import android.os.Bundle
+import android.content.Intent
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById(R.id.button) as Button
+        button.setOnClickListener {
+            val intent = Intent(this, ARCoreActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
