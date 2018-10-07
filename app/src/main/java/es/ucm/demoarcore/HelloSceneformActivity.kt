1a1e1a8d053db68473249dae1028ef88a71c97b5
Daniel Calle <danielcallesanchez@gmail.com>
2018-10-07 13:45:59 +0200

    Implementation of scene in kotlin

---
+++ app/src/main/java/es/ucm/demoarcore/HelloSceneformActivity.kt
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2018 Google LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package es.ucm.demoarcore
+
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+//import android.os.Build
+//import android.os.Build.VERSION_CODES
+import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.MotionEvent
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+//import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+
+
+
+class HelloSceneformActivity : AppCompatActivity() {
+    private val TAG = HelloSceneformActivity::class.java.simpleName
+    private val MIN_OPENGL_VERSION = 3.0
+
+    private var arFragment: ArFragment? = null
+    private var andyRenderable: ModelRenderable? = null
+
+    override// CompletableFuture requires api level 24
+    // FutureReturnValueIgnored is not valid
+    fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (!checkIsSupportedDeviceOrFinish(this)) {
+            return
+        }
+
+        setContentView(R.layout.activity_ux)
+        arFragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment?
+
+        // When you build a Renderable, Sceneform loads its resources in the background while returning
+        // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
+        ModelRenderable.builder()
+                .setSource(this, R.raw.andy)
+                .build()
+                .thenAccept{
+                    renderable -> andyRenderable = renderable
+                }
+                .exceptionally {
+                            throwable ->
+                            val toast = Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG)
+                            toast.setGravity(Gravity.CENTER, 0, 0)
+                            toast.show()
+                            return@exceptionally null
+                        }
+
+        arFragment!!.setOnTapArPlaneListener { hitResult: HitResult, plane: Plane, motionEvent: MotionEvent ->
+            if (andyRenderable != null) {
+                // Create the Anchor.
+                val anchor = hitResult.createAnchor()
+                val anchorNode = AnchorNode(anchor)
+                anchorNode.setParent(arFragment!!.arSceneView.scene)
+
+                // Create the transformable andy and add it to the anchor.
+                val andy = TransformableNode(arFragment!!.transformationSystem)
+                andy.setParent(anchorNode)
+                andy.renderable = andyRenderable
+                andy.select()
+            }
+        }
+    }
+
+    /**
+     * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
+     * on this device.
+     *
+     *
+     * Sceneform requires Android N on the device as well as OpenGL 3.0 capabilities.
+     *
+     *
+     * Finishes the activity if Sceneform can not run
+     */
+    private fun checkIsSupportedDeviceOrFinish(activity: Activity): Boolean {
+        /*
+        if (Build.VERSION.SDK_INT < VERSION_CODES.N) {
+            Log.e(TAG, "Sceneform requires Android N or later")
+            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show()
+            activity.finish()
+            return false
+        }
+        */
+        val openGlVersionString = (activity.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager)
+                .deviceConfigurationInfo
+                .glEsVersion
+        if (java.lang.Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later")
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show()
+            activity.finish()
+            return false
+        }
+        return true
+    }
+}
--- app/src/main/java/es/ucm/demoarcore/MainActivity.kt
+++ app/src/main/java/es/ucm/demoarcore/MainActivity.kt
@@ -14,9 +14,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById(R.id.button) as Button
+        val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
-            val intent = Intent(this, ARCoreActivity::class.java)
+            val intent = Intent(this, HelloSceneformActivity::class.java)
             startActivity(intent)
         }
     }
